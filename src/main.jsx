11ce21729d3895a3e56ad4ce23e5c79472b1101b
adnannawaz786-dotcom <adnan.nawaz786@ymail.com>
2025-08-12 13:08:56 +0100

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+```jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './styles/variables.css'
+import './styles/global.css'
+
+// Initialize the React application
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+```
