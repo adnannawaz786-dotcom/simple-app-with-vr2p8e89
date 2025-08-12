3d465b69cb720d8447a078b2d6f801f34177f4ee
adnannawaz786-dotcom <adnan.nawaz786@ymail.com>
2025-08-12 13:12:49 +0100

    Update src/main.jsx via IDE - manual edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-```jsx
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -11,4 +11,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
-```
